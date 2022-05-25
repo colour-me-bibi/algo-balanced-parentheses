a160c74213eeb1355918b0a526757c9ef7482e53
colour-me-bibi <bennyforeman1@gmail.com>
2022-05-25 16:25:21 -0700

    Add comments

--- python/balance_parens.py
+++ python/balance_parens.py
@@ -13,24 +13,32 @@ def balance_parens(string):  # TODO disgusting
     slow, fast = 0, 0
 
     while slow < len(string):
+        # if fast pointer out of bounds, reset stack and set pointers to slow + 1
         if fast > len(string) - 1:
             stack = 0
             fast = slow = slow + 1
             continue
 
+        # if c is ( increment the stack
         if string[fast] == "(":
             stack += 1
         elif string[fast] == ")":
-            if stack:
-                stack -= 1
-                if not stack:
-                    result += string[slow:fast + 1]
-                    slow = fast = fast + 1
-                    continue
-            else:
+            # if c is ) and there's no matching parenthesis, reset stack and set pointers to fast + 1
+            if not stack:
                 stack = 0
                 fast = slow = slow + 1
                 continue
+
+            stack -= 1
+
+            # if this ) emptied the stack, append the valid substring to the result
+            #     and set pointers to fast + 1
+            if not stack:
+                result += string[slow:fast + 1]
+                slow = fast = fast + 1
+                continue
+        
+        # capture characters outside of valid parenthesis
         elif not stack:
             result += string[fast]
             slow = fast = fast + 1
